255ad0b49301cc86c0f9225097af79b92db0880b
palax-99 <ninociao7@gmail.com>
2026-05-15 11:05:53 +0200

    Initial commit

---
+++ src/main/java/antoninopalazzolo/kitchensync/KitchensyncApplication.java
@@ -0,0 +1,13 @@
+package antoninopalazzolo.kitchensync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchensyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antoninopalazzolo/kitchensync/KitchensyncApplicationTests.java
@@ -0,0 +1,13 @@
+package antoninopalazzolo.kitchensync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchensyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
